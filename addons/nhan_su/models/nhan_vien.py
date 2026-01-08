3798ab529569ae7e18800b2c7b3e4aee4a3b878b
Nam Nguyen <truongnam0304@gmail.com>
2026-01-08 14:24:00 +0700

    cho manh love

--- addons/nhan_su/models/nhan_vien.py
+++ addons/nhan_su/models/nhan_vien.py
@@ -1,13 +1,18 @@
 from odoo import models, fields, api
 
-
 class NhanVien(models.Model):
     _name = 'nhan_vien'
     _description = 'Bảng chứa thông tin nhân viên'
-
     _rec_name = 'ten_nhan_vien'
 
-    ma_dinh_danh = fields.Char("Mã định danh", required=True)
+    # SỬA LỖI 1: Tên hàm compute phải khớp với tên hàm bên dưới
+    ma_dinh_danh = fields.Char(
+        "Mã định danh", 
+        required=True, 
+        store=True, 
+        compute='_compute_ma_dinh_danh' # Tên hàm cần khớp
+    )
+    
     ten_nhan_vien = fields.Char("Tên nhân viên", required=True)
     ngay_sinh = fields.Date("Ngày sinh")
     que_quan = fields.Char("Quê quán")
@@ -16,8 +21,56 @@ class NhanVien(models.Model):
     dia_chi = fields.Text("Địa chỉ")
     luong = fields.Float("Lương")
     bao_hiem_xa_hoi = fields.Char("Bảo hiểm xã hội")
+    
+    # CẢNH BÁO: Đảm bảo bạn đã có model 'chuc_vu' trong hệ thống
     chuc_vu_id = fields.Many2one('chuc_vu', string="Chức vụ hiện tại")
 
+    # CẢNH BÁO: Đảm bảo các model này đã tồn tại
     lich_su_ids = fields.One2many('lich_su_cong_tac', 'nhan_vien_id', string="Lịch sử công tác")
     chung_chi_ids = fields.One2many('chung_chi', 'nhan_vien_id', string="Danh sách chứng chỉ")
     cham_cong_ids = fields.One2many('cham_cong', 'nhan_vien_id', string="Bảng chấm công")
+
+    # SỬA LỖI 1: Dùng api.depends thay vì onchange cho field compute
+    @api.depends('ten_nhan_vien', 'ngay_sinh')
+    def _compute_ma_dinh_danh(self):
+        for rec in self:
+            if rec.ten_nhan_vien and rec.ngay_sinh:
+                cac_tu = rec.ten_nhan_vien.strip().split()
+                # Thêm kiểm tra để tránh lỗi nếu tên nhập toàn dấu cách
+                if cac_tu:
+                    chu_cai_dau = "".join([w[0].upper() for w in cac_tu])
+                    ngay_sinh_str = rec.ngay_sinh.strftime("%Y%m%d")
+                    rec.ma_dinh_danh = f'{chu_cai_dau}{ngay_sinh_str}'
+                else:
+                    rec.ma_dinh_danh = False
+            else:
+                rec.ma_dinh_danh = False
+
+    # van_ban_den_ids = fields.One2many(
+    #     'van_ban_den',
+    #     'nhan_vien_xu_ly_id',
+    #     string="Văn bản phụ trách"
+    # )
+
+    # van_ban_den_count = fields.Integer(
+    #     compute="_compute_vb_count"
+    # )
+    
+    # def _compute_vb_count(self):
+    #     for rec in self:
+    #         # Code này đúng, nhưng có thể viết ngắn gọn hơn:
+    #         # rec.van_ban_den_count = len(rec.van_ban_den_ids)
+    #         rec.van_ban_den_count = self.env['van_ban_den'].search_count([
+    #             ("nhan_vien_xu_ly_id", "=", rec.id)
+    #         ])
+
+    # def action_open_van_ban_den(self):
+    #     self.ensure_one()
+    #     return {
+    #         "type": "ir.actions.act_window",
+    #         "name": "Văn bản đến",
+    #         "res_model": "van_ban_den",
+    #         "view_mode": "tree,form",
+    #         "domain": [("nhan_vien_xu_ly_id", "=", self.id)],
+    #         "context": {'default_nhan_vien_xu_ly_id': self.id} # Thêm dòng này để khi tạo mới sẽ tự điền tên nhân viên
+    #     }
--- addons/quan_ly_van_ban/__manifest__.py
+++ addons/quan_ly_van_ban/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "nhan_su",
+    'name': "quan_ly_van_ban",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "FIT-DNU",
+    'website': "https://ttdn1501.aiotlabdnu.xyz/web",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,21 +20,18 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-    'installable': True,
-    'application': True,
+    'depends': ['base', 'nhan_su'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/nhan_vien.xml',
-        'views/phong_ban.xml',
-        'views/chuc_vu.xml',
-        'views/lich_su_cong_tac.xml',
-        'views/chung_chi.xml',
-        'views/cham_cong.xml',
+        'views/van_ban_den.xml',
+        'views/van_ban_di.xml',
+        'views/loai_van_ban.xml',
         'views/menu.xml',
     ],
+    'installable': True,
+    'application': True,
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
